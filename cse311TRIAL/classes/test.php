646fa5e1c606c336aa07ac8e3f544222621cc3ee
ZahinSam1 <toky.jahin@gmail.com>
2022-08-24 15:39:34 +0600

    added testing file

--- cse311TRIAL/classes/test.php
+++ cse311TRIAL/classes/test.php
@@ -1,21 +1,21 @@
 <?php
-    // require 'connectionClass.php';
-    //require 'DonorClass.php';
-    require 'DBFunctionClass.php';
+    require 'connectionClass.php';
 
-    //$admin1 = new Admin('Ruhama', 'CEO');
-    //$admin1 = new Donor('ruhama', 'B+');
-    // $connect = new connection();
+    $db = new connection();
+    $con = $db->connect();
+    $query = "SELECT Employee_ID, First_Name FROM employees";
 
-    $func = new DBFunctions();
+    $result = mysqli_query($con, $query);
 
+    //$info = mysqli_fetch_assoc($result);
+
+    if(mysqli_num_rows($result)){
+        while($info = mysqli_fetch_assoc($result)){
+            echo $info['Employee_ID'] . "      " . $info['First_Name'];
+            echo '<br>';
+        }
+    }
     
 
-    // echo $admin1->getName();
-    // echo "<br>";
-    // echo $admin1->getDesignation();
-    // echo "<br>";
-    // //$admin1->setBloodGroup("B+");
-    // echo $admin1->getBLoodGroup();
 
 ?>
